e9c1abf1ec4c629eac093750b250544cf6922eba
Premila Anand <panand71@gmail.com>
2015-05-03 17:15:47 -0700

    adding helpers and tests for dynamic page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Fashion Truck US"
+    assert_select "title", "Fashion Truck US"
   end
 
   test "should get help" do
